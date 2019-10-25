c59cd0c92a316629c4a2418586258166a4481f1f
Sebastian Bergman <54362944+bgk298@users.noreply.github.com>
2019-10-25 18:10:27 +0300

    Add files via upload

---
+++ 2. Moduuli.js
@@ -0,0 +1,17 @@
+function tulostaTahtia(tmaara) {
+    let text = "";
+    let tahtia = "* ";
+    for (i = 0; i < tmaara; i++) {
+        text += tahtia;
+    }
+    console.log(text);
+}
+function tulostaTulos(tulostaT) {
+    console.log(tulostaT)
+}
+function tulostaTulos(tulostaN) {
+    console.log(tulostaN);
+}
+tulostaTahtia(10);
+tulostaTulos("Tämä on lopputulos");
+tulostaTulos(34523452);
